3cf5f343ff677ca51094428f1587edc2f202a493
KKAgarwal007 <darkside992901@gmail.com>
2025-07-13 10:22:52 +0530

    Update script.js

--- backend/script.js
+++ backend/script.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://medvault-frontend-u7i7.onrender.com",
     credentials: true
 }))
 const port = process.env.PORT || 5000
